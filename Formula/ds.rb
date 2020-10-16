018ce209f90ac1cc0ecf049a70804611b0f858ae
Dominik Víšek <visekdo@gmail.com>
2020-10-16 23:18:16 +0200

    test

---
+++ Formula/ds.rb
@@ -0,0 +1,28 @@
+class Ds < Formula
+  desc "test"
+  url "https://github.com/DominikVisek/commands/archive/v0.0.1.tar.gz"
+  sha256 "3ad3bfb6b8b57d56a4404b657dc72d51789256403bce5fdcdd9e8f67dcab526c"
+
+  depends_on "go" => :build
+
+  bottle do
+    root_url "https://github.com/DominikVisek/commands/releases/tag/v0.0.1"
+    cellar :any_skip_relocation
+  end
+
+  def install
+      ENV["GOPATH"] = buildpath
+
+      bin_path = buildpath/"src/github.com/DominikVisek/commands/cmd/ds"
+
+      bin_path.install Dir["*"]
+
+      cd bin_path do
+            system "go", "build", "-o", bin/"ds", "."
+      end
+  end
+
+  test do
+    system "#{bin}/ds", "--version"
+  end
+end
